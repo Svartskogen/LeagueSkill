a7754e14bdf89c319bc7a387a9a80fad6865e353
Svartskogen <amichelidebard@gmail.com>
2021-03-26 23:17:20 -0300

    Agregar archivos de proyecto.

---
+++ LeagueSkill/Pages/Index.cshtml.cs
@@ -0,0 +1,56 @@
+﻿using LeagueSkill.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using RiotNet;
+using RiotNet.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LeagueSkill.Pages
+{
+    public class IndexModel : PageModel
+    {
+        [BindProperty(SupportsGet = true)]
+
+        public ProfileQuery Profile { get; set; }
+        public bool ValidSearch { get; private set; } //true si es valido el parametro de busqueda
+        public bool DataLoaded { get; private set; } //true si encontro el invocador
+
+        //Model
+        public Summoner SummonerInfo { get; set; }
+        public List<LeagueEntry> Leagues { get; set; }
+
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            ValidSearch = false;
+            DataLoaded = false;
+        }
+        public IActionResult OnPost()
+        {
+            if (!string.IsNullOrWhiteSpace(Profile.Username))
+            {
+                ValidSearch = true;
+                //buscar perfil
+                var api = new RiotClient(new RiotClientSettings
+                {
+                    ApiKey = "RGAPI-a6614a66-8fd2-48b3-ad30-a7137d3b7d57"
+                });
+                SummonerInfo = Utils.GetSummonerData(api, Profile.Username).Result;
+                Leagues = Utils.GetLeagues(api, SummonerInfo).Result;
+
+                DataLoaded = SummonerInfo != null;
+            }
+            return Page();
+        } 
+    }
+}
---
+++ LeagueSkill/Utils.cs
@@ -0,0 +1,27 @@
+﻿using RiotNet;
+using RiotNet.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LeagueSkill
+{
+    public static class Utils
+    {
+        public static async Task<Summoner> GetSummonerData(RiotClient api, string username)
+        {
+            Summoner summoner = await api.GetSummonerBySummonerNameAsync(username, PlatformId.LA2).ConfigureAwait(false);
+            return summoner;
+        }
+        public static async Task<List<LeagueEntry>> GetLeagues(RiotClient api, Summoner summoner)
+        {
+            List<LeagueEntry> list = await api.GetLeagueEntriesBySummonerIdAsync(summoner.Id, PlatformId.LA2).ConfigureAwait(false);
+            return list;
+        }
+        public static string GetLeagueData(LeagueEntry entry)
+        {
+            return entry.QueueType + " "  + entry.Tier + " " + entry.Rank + " " + entry.LeaguePoints + "LP " + ((float)entry.Wins/entry.Losses * 50f).ToString() + "%WR ";
+        }
+    }
+}
